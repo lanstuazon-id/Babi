15456b85c80cf07df774cd7e504356eddc090454
Lhance Tuazon <lhancetuazon0819@icloud.com>
2025-12-12 13:49:00 +0800

    updated files and added files

---
+++ service-worker.js
@@ -0,0 +1,13 @@
+self.addEventListener("install", (event) => {
+  console.log("Service Worker installed");
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  console.log("Service Worker activated");
+});
+
+self.addEventListener("fetch", (event) => {
+  // Basic PWA fetch handler (app stays installable)
+  event.respondWith(fetch(event.request));
+});
